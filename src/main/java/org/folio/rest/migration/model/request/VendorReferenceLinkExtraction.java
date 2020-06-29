d1115dcb6c434d49b18c436a1e7aceccdc3fed10
William Welling <wwelling@library.tamu.edu>
2020-06-29 09:51:06 -0500

    no additional sql needed for vendor extraction

--- src/main/java/org/folio/rest/migration/model/request/VendorReferenceLinkExtraction.java
+++ src/main/java/org/folio/rest/migration/model/request/VendorReferenceLinkExtraction.java
@@ -1,22 +1,9 @@
 package org.folio.rest.migration.model.request;
 
-import javax.validation.constraints.NotNull;
-
 public class VendorReferenceLinkExtraction extends AbstractExtraction {
-
-  @NotNull
-  private String vendorSql;
 
   public VendorReferenceLinkExtraction() {
     super();
-  }
-
-  public String getVendorSql() {
-    return vendorSql;
-  }
-
-  public void setVendorSql(String vendorSql) {
-    this.vendorSql = vendorSql;
   }
 
 }
