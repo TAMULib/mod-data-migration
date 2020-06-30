8e5d47ba298584e8f1881d5965ce0ca26d28e54a
William Welling <wwelling@library.tamu.edu>
2020-06-30 10:12:47 -0500

    remove unused and now inaccurate stream grouping methods

--- src/main/java/org/folio/rest/controller/ReferenceLinkController.java
+++ src/main/java/org/folio/rest/controller/ReferenceLinkController.java
@@ -8,23 +8,17 @@ import java.util.stream.Stream;
 import org.folio.rest.model.ReferenceLink;
 import org.folio.rest.model.repo.ReferenceLinkRepo;
 import org.folio.rest.model.response.BatchReport;
-import org.folio.rest.model.response.CollectorReferenceLink;
-import org.folio.rest.model.response.JoinReferenceLink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/referenceLinks")
@@ -38,8 +32,8 @@ public class ReferenceLinkController {
   @Value("${data-extractor.batch.partition-size:10}")
   public int partitionSize;
 
-  @PostMapping("/batch")
   @Transactional
+  @PostMapping("/batch")
   public BatchReport batchCreate(@RequestBody List<ReferenceLink> referenceLinks) throws IOException {
     long startTime = System.nanoTime();
     batches(referenceLinks, partitionSize).parallel().forEach(batch -> referenceLinkRepo.saveAll(batch));
@@ -56,31 +50,6 @@ public class ReferenceLinkController {
     return referenceLinkRepo.findAllByTypeNameAndExternalReferenceIn(typeName, externalReferences);
   }
 
-  @GetMapping(value = "/stream/{typeId}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-  public Flux<ReferenceLink> streamAllByTypeId(@PathVariable String typeId,
-      @RequestParam(defaultValue = "Integer") String orderClass) throws IOException, ClassNotFoundException {
-    logger.info("Streaming all ExternalReference by type id {} order by class {}", typeId, orderClass);
-    return Flux.fromStream(referenceLinkRepo.streamAllByTypeIdOrderByExternalReferenceAsc(typeId, orderClass));
-  }
-
-  @GetMapping(value = "/stream/{typeId}/collect/{collectTypeId}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-  public Flux<CollectorReferenceLink> streamAllByTypeIdCollectingTypeId(@PathVariable String typeId,
-      @PathVariable String collectTypeId, @RequestParam(defaultValue = "Integer") String orderClass)
-      throws IOException, ClassNotFoundException {
-    logger.info("Streaming all ExternalReference by type id {} collect type id {}", typeId, collectTypeId);
-    return Flux.fromStream(referenceLinkRepo.streamAllByTypeIdCollectingTypeIdOrderByExternalReferenceAsc(typeId,
-        collectTypeId, orderClass));
-  }
-
-  @GetMapping(value = "/stream/{typeId}/join/{joinTypeId}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-  public Flux<JoinReferenceLink> streamAllByTypeIdJoiningTypeId(@PathVariable String typeId,
-      @PathVariable String joinTypeId, @RequestParam(defaultValue = "Integer") String orderClass)
-      throws IOException, ClassNotFoundException {
-    logger.info("Streaming all ExternalReference by type id {} join type id {}", typeId, joinTypeId);
-    return Flux.fromStream(
-        referenceLinkRepo.streamAllByTypeIdJoiningTypeIdOrderByExternalReferenceAsc(typeId, joinTypeId, orderClass));
-  }
-
   public static <T> Stream<List<T>> batches(List<T> source, int length) {
     if (length <= 0) {
       throw new IllegalArgumentException("length = " + length);
@@ -91,7 +60,7 @@ public class ReferenceLinkController {
     }
     int fullChunks = (size - 1) / length;
     return IntStream.range(0, fullChunks + 1)
-        .mapToObj(n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
+      .mapToObj(n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
   }
 
 }
--- src/main/java/org/folio/rest/model/repo/ReferenceLinkRepo.java
+++ src/main/java/org/folio/rest/model/repo/ReferenceLinkRepo.java
@@ -7,10 +7,9 @@ import org.folio.rest.model.ReferenceLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource
-public interface ReferenceLinkRepo extends JpaRepository<ReferenceLink, String>, ReferenceLinkRepoCustom {
+public interface ReferenceLinkRepo extends JpaRepository<ReferenceLink, String> {
 
   public Optional<ReferenceLink> findByTypeIdAndExternalReference(@Param("typeId") String typeId,
       @Param("externalReference") String externalReference);
@@ -35,8 +34,5 @@ public interface ReferenceLinkRepo extends JpaRepository<ReferenceLink, String>,
 
   public Optional<ReferenceLink> findByTypeIdAndExternalReferenceAndFolioReference(@Param("typeId") String typeId,
       @Param("externalReference") String externalReference, @Param("folioReference") String folioReference);
-
-  @Transactional
-  public long deleteByTypeName(@Param("typeName") String typeName);
 
 }
