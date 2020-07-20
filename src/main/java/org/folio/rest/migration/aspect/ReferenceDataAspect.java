3077891bae3294313025d00baf1b4c821a268a1f
William Welling <wwelling@library.tamu.edu>
2020-07-20 14:07:47 -0500

    update mapping rules before bibs migration

--- src/main/java/org/folio/rest/migration/aspect/ReferenceDataAspect.java
+++ src/main/java/org/folio/rest/migration/aspect/ReferenceDataAspect.java
@@ -31,7 +31,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReferenceDataAspect {
 
-  private final static Logger logger = LoggerFactory.getLogger(ReferenceLinkTypeAspect.class);
+  private final static Logger logger = LoggerFactory.getLogger(ReferenceDataAspect.class);
 
   @Autowired
   private OkapiService okapiService;
