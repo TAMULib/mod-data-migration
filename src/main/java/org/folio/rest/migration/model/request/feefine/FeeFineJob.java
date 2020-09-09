eaef05a81c55860b7a7583067cd3d9065d8bfe17
William Welling <wwelling@library.tamu.edu>
2020-09-09 12:31:33 -0500

    add references to fee fine job

--- src/main/java/org/folio/rest/migration/model/request/feefine/FeeFineJob.java
+++ src/main/java/org/folio/rest/migration/model/request/feefine/FeeFineJob.java
@@ -1,7 +1,23 @@
 package org.folio.rest.migration.model.request.feefine;
 
+import java.util.Map;
+
 import org.folio.rest.migration.model.request.AbstractJob;
 
 public class FeeFineJob extends AbstractJob {
+
+  private Map<String, String> references;
+
+  public FeeFineJob() {
+
+  }
+
+  public Map<String, String> getReferences() {
+    return references;
+  }
+
+  public void setReferences(Map<String, String> references) {
+    this.references = references;
+  }
 
 }
