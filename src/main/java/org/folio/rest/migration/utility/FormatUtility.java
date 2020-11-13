c7e6a1078651e8f91967db984f28b7d5f8bf12cd
William Welling <wwelling@library.tamu.edu>
2020-11-13 11:06:55 -0600

    remove unused import

--- src/main/java/org/folio/rest/migration/utility/FormatUtility.java
+++ src/main/java/org/folio/rest/migration/utility/FormatUtility.java
@@ -1,7 +1,5 @@
 package org.folio.rest.migration.utility;
 
-import java.util.Locale;
-
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
