458b2fe90d120e2218e15b07b05f01ae54179fba
William Welling <wwelling@library.tamu.edu>
2021-01-06 15:37:54 -0600

    create copy holdings record for child instance that does not have one

--- src/main/java/org/folio/rest/migration/BoundWithMigration.java
+++ src/main/java/org/folio/rest/migration/BoundWithMigration.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import org.folio.rest.jaxrs.model.inventory.Holdingsrecord;
+import org.folio.rest.jaxrs.model.inventory.Holdingsrecords;
 import org.folio.rest.jaxrs.model.inventory.Instance;
 import org.folio.rest.jaxrs.model.inventory.Instancerelationship;
 import org.folio.rest.jaxrs.model.inventory.Item;
@@ -181,6 +182,29 @@ public class BoundWithMigration extends AbstractMigration<BoundWithContext> {
           }
 
           for (ReferenceLink instanceRL : instanceRLs) {
+            Holdingsrecords holdingsRecords = migrationService.okapiService.fetchHoldingsRecordsByInstanceId(tenant, token, instanceRL.getFolioReference());
+            boolean hasExistingHoldingsRecord = false;
+            for (Holdingsrecord childHoldingsRecord : holdingsRecords.getHoldingsRecords()) {
+              if (childHoldingsRecord.getId().equals(existingHoldingsRecord.getId())) {
+                hasExistingHoldingsRecord = true;
+                break;
+              }
+            }
+            if (!hasExistingHoldingsRecord) {
+              Holdingsrecord childHoldingsRecord = existingHoldingsRecord;
+              String bibId = instanceRL.getExternalReference();
+              childHoldingsRecord.setId(craftUUID("bound-with-child-holdings-record", schema, mfhdId + ":" + bibId));
+              childHoldingsRecord.setInstanceId(instanceRL.getFolioReference());
+              try {
+                migrationService.okapiService.postHoldingsrecord(tenant, token, childHoldingsRecord);
+              } catch (Exception e) {
+                log.error("failed to create duplicate child holdings record for child instance {}: {}", instanceRL.getFolioReference(), e.getMessage());
+                continue;
+              }
+            }
+          }
+
+          for (ReferenceLink instanceRL : instanceRLs) {
             Instancerelationship instanceRelationship = new Instancerelationship();
             instanceRelationship.setSuperInstanceId(parentInstance.getId());
             instanceRelationship.setSubInstanceId(instanceRL.getFolioReference());
@@ -194,13 +218,15 @@ public class BoundWithMigration extends AbstractMigration<BoundWithContext> {
           }
 
           Holdingsrecord parentHoldingsRecord = new Holdingsrecord();
+
           parentHoldingsRecord.setId(craftUUID("bound-with-parent-holdings-record", schema, mfhdId));
-          parentHoldingsRecord.setFormerIds(existingHoldingsRecord.getFormerIds());
-          parentHoldingsRecord.setCallNumber(existingHoldingsRecord.getCallNumber());
           parentHoldingsRecord.setInstanceId(parentInstance.getId());
           parentHoldingsRecord.setHoldingsTypeId(job.getHoldingsTypeId());
-          parentHoldingsRecord.setCallNumberTypeId(existingHoldingsRecord.getCallNumberTypeId());
           parentHoldingsRecord.setDiscoverySuppress(true);
+
+          parentHoldingsRecord.setFormerIds(existingHoldingsRecord.getFormerIds());
+          parentHoldingsRecord.setCallNumber(existingHoldingsRecord.getCallNumber());
+          parentHoldingsRecord.setCallNumberTypeId(existingHoldingsRecord.getCallNumberTypeId());
           parentHoldingsRecord.setPermanentLocationId(existingHoldingsRecord.getPermanentLocationId());
 
           try {
