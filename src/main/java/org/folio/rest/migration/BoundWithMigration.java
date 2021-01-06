21b4f8893b7506af651c7c4b3f30fcd0eed6fe37
William Welling <wwelling@library.tamu.edu>
2021-01-06 13:28:14 -0600

    remove unused import

--- src/main/java/org/folio/rest/migration/BoundWithMigration.java
+++ src/main/java/org/folio/rest/migration/BoundWithMigration.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import org.folio.rest.jaxrs.model.inventory.Holdingsrecord;
